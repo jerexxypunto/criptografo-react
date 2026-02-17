55e691c2888b4c391375f7d19c752fa9bb90d7a8
Jeremías San Martín <jeremias@reevolutiva.com~>
2026-02-17 10:10:38 -0300

    save

---
+++ host.js
@@ -0,0 +1,31 @@
+
+const redirectUrl = {
+    'app': 'dist/',
+};
+
+const relateivePath = [
+    '/secret-note-scribe/'
+];
+
+function redriectTo( path ){
+
+    function path_compare( route ){
+
+        const url = new URL(location.href);
+        const pathname = url.pathname;
+
+        if (pathname === '/index.html' || pathname === '/' || pathname === route ) {
+            return true;
+        }else{
+            return false;
+        }
+
+    }
+
+    if (path_compare( path )) {
+        // Redirect to the app page
+        location.href = path + redirectUrl.app;
+    }
+}
+
+relateivePath.forEach( path => redriectTo(path) );
